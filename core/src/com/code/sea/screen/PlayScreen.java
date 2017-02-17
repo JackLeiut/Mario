be591ed16f7d8b59fc07dd822f7bebfe98cb0377
JackLeiu <895896736@qq.com>
2017-02-17 09:30:30 +0800

    add map box2d

--- core/src/com/code/sea/screen/PlayScreen.java
+++ core/src/com/code/sea/screen/PlayScreen.java
@@ -4,10 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.code.sea.MarioBros;
@@ -25,6 +35,9 @@ public class PlayScreen implements Screen {
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
 
 
@@ -47,7 +60,67 @@ public class PlayScreen implements Screen {
 
         //initially set our gamcam to be centered correctly at the start of of map
         gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+        
+        world = new World(new Vector2(0,0),true);
+        b2dr = new Box2DDebugRenderer();
+        
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body ;
+        
+        for(MapObject object:map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject)object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX()+rect.getWidth()/2,rect.getY()+rect.getHeight()/2);
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
+        }
+        
+        for(MapObject object:map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject)object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX()+rect.getWidth()/2,rect.getY()+rect.getHeight()/2);
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
 
+        }
+        
+        for(MapObject object:map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject)object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX()+rect.getWidth()/2,rect.getY()+rect.getHeight()/2);
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
+
+        }
+        
+        for(MapObject object:map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject)object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX()+rect.getWidth()/2,rect.getY()+rect.getHeight()/2);
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
+        }
+        
+        
+        
     }
 
     @Override
@@ -88,7 +161,8 @@ public class PlayScreen implements Screen {
 
         //render our game map
         renderer.render();
-
+        
+        b2dr.render(world, gamecam.combined);
 
         //Set our batch to now draw what the Hud camera sees.
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
